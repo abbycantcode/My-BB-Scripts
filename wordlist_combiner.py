c1d5ede6e4c43546fe76f619e529b5ce9e0a0571
abbycantcode <98319206+abbycantcode@users.noreply.github.com>
2023-03-23 13:02:44 +0530

    Updated wordlist_combiner.py for multithreading

--- wordlist_combiner.py
+++ wordlist_combiner.py
@@ -1,11 +1,13 @@
 import sys
+import threading
 
-if len(sys.argv) != 3:
-    print("Usage: python script.py file1.txt file2.txt")
+if len(sys.argv) != 4:
+    print("Usage: python wordlist_combiner.py user.txt pass.txt num_threads")
     sys.exit(1)
 
 file1 = sys.argv[1]
 file2 = sys.argv[2]
+num_threads = int(sys.argv[3])
 outfile = "combined-wordlist.txt"
 
 words1 = []
@@ -18,7 +20,24 @@ with open(file2, "r") as f2:
     for line in f2:
         words2.append(line.strip())
 
-with open(outfile, "w") as f_out:
-    for word1 in words1:
-        for word2 in words2:
-            f_out.write("{}:{}\n".format(word1, word2))
+lock = threading.Lock()
+
+def write_permutations(words):
+    with open(outfile, "a") as f_out:
+        for word1 in words:
+            for word2 in words2:
+                line = "{}:{}\n".format(word1, word2)
+                with lock:
+                    f_out.write(line)
+
+# Dividing the words into chunks for each thread
+word_chunks = [words1[i::num_threads] for i in range(num_threads)]
+
+threads = []
+for chunk in word_chunks:
+    thread = threading.Thread(target=write_permutations, args=(chunk,))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
