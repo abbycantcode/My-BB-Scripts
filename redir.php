ed3424bd5446e92c3c39812b7e662f2e19e4747a
Abhinav Jha <98319206+syntaxswindler@users.noreply.github.com>
2023-07-26 11:10:21 +0530

    Create redir.php
    A redirect script to host on a server if needed a redirect for SSRFs

---
+++ redir.php
@@ -0,0 +1,21 @@
+Copy code
+<?php
+// Get the URL parameter from the query string
+$url = $_GET['url'];
+
+// Get the status code parameter from the query string, default to 302 if not provided
+$statusCode = isset($_GET['code']) ? intval($_GET['code']) : 302;
+
+// List of supported status codes
+$supportedStatusCodes = array(301, 302, 303, 307);
+
+// Check if the provided status code is supported
+if (!in_array($statusCode, $supportedStatusCodes)) {
+    // If the provided status code is not supported, default to 302
+    $statusCode = 302;
+}
+
+// Perform the redirect with the provided status code
+header("Location: $url", true, $statusCode);
+exit();
+?>
